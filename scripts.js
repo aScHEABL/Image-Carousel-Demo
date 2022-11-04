aca5b88253cf4789b6bf256f8d1135e64153fe00
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2022-11-04 20:30:33 +0800

    feat: image slider buttons

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+const buttons = document.querySelectorAll("[data-carousel-button]");
 let newIndex = 0;
 
 function autoSlider() {
@@ -11,5 +12,22 @@ function autoSlider() {
     slides.children[newIndex].dataset.active = true;
     delete activeSlide.dataset.active;
 }
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+        const slides = document
+        .querySelector("[data-carousel]")
+        .querySelector("[data-slides]");
+
+        const activeSlide = document.querySelector("[data-active]");
+        newIndex = [...slides.children].indexOf(activeSlide) + offset;
+        if (newIndex < 0) newIndex = slides.children.length - 1;
+        if (newIndex >= slides.children.length) newIndex = 0;
+
+        slides.children[newIndex].dataset.active = true;
+        delete activeSlide.dataset.active;
+    })    
+})
 
 setInterval(autoSlider, 3000);
